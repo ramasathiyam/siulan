7c762e37b18b54302a0e77a8952c238d04a5ff23
Rama Sathiyam <ramasathiyam1235@gmail.com>
2025-07-11 08:28:07 +0800

    menambahkan pencarian

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -22,17 +22,12 @@
             <li><a href="#" class="nav-link active text-white">Kelola Postingan</a></li>
         </ul>
         <hr>
-        <div class="dropdown">
-            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-                <strong>Admin</strong>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                <li><a class="dropdown-item" href="#">Profile</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="{{ route('login') }}">Sign out</a></li>
-            </ul>
-        </div>
+        
+        <form action="{{ route('logout') }}" method="POST" class="d-inline">
+            @csrf
+            <button type="submit" class="btn btn-danger ms-3">Logout</button>
+        </form>
+
     </div>
 
     <!-- Main Content -->
